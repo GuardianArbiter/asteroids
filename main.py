b9ba7022f93ab8987bc5798c1b815548d0188603
GuardianArbiter <greghill91@hotmail.com>
2025-05-29 10:21:37 +0100

    now with Player Sprite

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    num = 1
-    while num > 0:
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
     
         screen.fill(color = 000000)
+        player.draw(screen)
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
+
+    pygame.quit()
+        
 
 if __name__ == "__main__":
     main()
